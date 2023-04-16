611be8e704fe86bcabce72c894fd32349f1c7eca
suraj548 <surajgudaji548@gmail.com>
2023-04-16 17:28:44 +0530

    added filtering

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,14 @@
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DonorsComponent } from './display-donors/donors/donors.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppMaterialModule } from './app.material-module';
-
+import { MatTableModule } from '@angular/material/table';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormField, MatFormFieldModule } from '@angular/material/form-field';
 @NgModule({
   declarations: [
     AppComponent,
@@ -18,7 +19,10 @@ import { AppMaterialModule } from './app.material-module';
     AppRoutingModule,
     HttpClientModule,
     BrowserAnimationsModule,
-    AppMaterialModule
+    AppMaterialModule,
+    MatFormFieldModule,
+    MatTableModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/display-donors/donors/donors.component.ts
+++ src/app/display-donors/donors/donors.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
 import { Donor } from 'src/app/donor';
 import { DonorsService } from 'src/app/donors.service';
+
 
 @Component({
   selector: 'app-donors',
@@ -14,11 +15,14 @@ export class DonorsComponent implements OnInit {
   public displayedColumns = ['Name', 'Address', 'Amount' ];
   public dataSource = new MatTableDataSource<Donor>();
   public sum:number=0
+
+  
   constructor(public donorList:DonorsService) { }
 
   ngOnInit(): void {
     this.donorList.getDonors().subscribe((all)=>{
       var i:number=0
+      console.log(all)
       this.donors=all
       this.dataSource.data=all
       console.log(all[0].name)
@@ -33,5 +37,12 @@ export class DonorsComponent implements OnInit {
     }
   )
 }
+
+filterData($event:any){
+  this.dataSource.filter=$event.target.value;
+}
+// public applyFilter(value: string) {
+//   this.dataSource.filter = value.trim().toLowerCase();
+// }
 
 }
